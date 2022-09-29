64bf09719d3d7180caf3f6354d433647d7d1fc22
SnowFox-95 <321mts95@gmail.com>
2022-09-29 11:46:03 +0300

    system_calculation + bin -> dec

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
 import time
 
+list_system = ['hex', 'oct', 'qua', 'bin']
+
 
 def is_valid_system(n_sys):
-    list_system = ['hex', 'oct', 'qua', 'bin']
+    global list_system
     if n_sys in list_system:
         return True
 
@@ -21,3 +23,49 @@ def interface_calc():
     print('\r', end='')
     time.sleep(1)
     print("Готово!")
+
+
+def calculation(dig, n_sys):
+    global list_system
+    if n_sys == list_system[0]:
+        rez = calc_hex(dig)
+        return int(rez)
+    elif n_sys == list_system[1]:
+        rez = calc_oct(dig)
+        return int(rez)
+    elif n_sys == list_system[2]:
+        rez = calc_qua(dig)
+        return int(rez)
+    else:
+        rez = calc_bin(dig)
+        return int(rez)
+
+
+def calc_bin(dig):
+    len_dig = len(dig)
+    summ = 0
+    for i in range(1, len_dig + 1):
+        digit = int(dig[-i])
+        summ += int(digit * (2 ** (i - 1)))
+    return summ
+
+
+def calc_qua(dig):
+    len_dig = len(dig)
+    summ = 0
+    # TODO transfer qua -> dec
+    return summ
+
+
+def calc_oct(dig):
+    len_dig = len(dig)
+    summ = 0
+    # TODO transfer oct -> dec
+    return summ
+
+
+def calc_hex(dig):
+    len_dig = len(dig)
+    summ = 0
+    # TODO transfer hex -> dec
+    return summ
