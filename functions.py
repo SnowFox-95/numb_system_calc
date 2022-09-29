67ce5afb3f508f637c7ab2dcb7d1b36192a0a4d8
SnowFox-95 <321mts95@gmail.com>
2022-09-29 10:52:27 +0300

    first interface + first fool protect

--- functions.py
+++ functions.py
@@ -1,18 +1,23 @@
-import functions
+import time
 
-if __name__ == "__main__":
-    print(
-        "Привет! Я калькулятор систем счисления. Помогу перевести любое число в десятичную систему.\n"
-        "Загадай число для перевода:"
-    )
-    digit = input()
-    print(
-        "Замечательно! \n"
-        "А в какой системе счислений изначально это число? \n"
-        "\n"
-        "HEX - шестнадцатеричная, \n"
-        "OCT - восмеричная, \n"
-        "QUA - четверичная, \n"
-        "BIN - бинарная(двоичная).\n"
-    )
-    system_calc = input()
+
+def is_valid_system(n_sys):
+    list_system = ['hex', 'oct', 'qua', 'bin']
+    if n_sys in list_system:
+        return True
+
+
+def interface_calc():
+    print("Сейчас посчитаю", end='')
+    print('\r', end='')
+    time.sleep(1)
+    print("Сейчас посчитаю.", end='')
+    print('\r', end='')
+    time.sleep(1)
+    print("Сейчас посчитаю..", end='')
+    print('\r', end='')
+    time.sleep(1)
+    print("Сейчас посчитаю...", end='')
+    print('\r', end='')
+    time.sleep(1)
+    print("Готово!")
